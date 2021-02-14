b77c9d98df81c94810499f4f99eb778fd965bc5c
aohara-cc <aohara@camcloud.com>
2021-02-14 13:50:05 -0500

    get API_HOST from environment

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const API_HOST = 'http://localhost:8000'
+export const API_HOST = process.env.REACT_APP_API_HOST
 export const STEAM_LOGIN_URL = `${API_HOST}/v1/auth/steam/login`
 export const OPENXBL_LOGIN_URL = `${API_HOST}/v1/auth/openxbl/login`
 export const APP_NAME = 'CheetosBros'
