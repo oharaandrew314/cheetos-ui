760c7ca00d714be280f607794a8755ded741e3a5
aohara-cc <aohara@camcloud.com>
2021-03-20 15:45:27 -0400

    Change Header name

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export const API_HOST = process.env.REACT_APP_API_HOST
 export const STEAM_LOGIN_URL = `${API_HOST}/v1/auth/steam/login`
 export const OPENXBL_LOGIN_URL = `${API_HOST}/v1/auth/openxbl/login`
-export const APP_NAME = 'Cheetos Bros'
+export const APP_NAME = 'Cheetos'
 export const APP_DESCRIPTION = 'Achievement Tracker for Steam'
